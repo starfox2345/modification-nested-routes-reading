029160fe86546e011e8faa924ca51c2178db259b
yatsun <yatsunlam25@gmail.com>
2021-07-26 01:07:00 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,8 +17,12 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
-  end
+    if params[:author_id] && !Author.exists?(params[:author_id])
+      redirect_to authors_path, alert: "Author not found."
+    else
+      @post = Post.new(author_id: params[:author_id])
+    end
+  end 
 
   def create
     @post = Post.new(post_params)
@@ -30,15 +34,25 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to post_path(@post)
-  end
+  end 
 
   def edit
-    @post = Post.find(params[:id])
-  end
+    if params[:author_id]
+      author = Author.find_by(id: params[:author_id])
+      if author.nil?
+        redirect_to authors_path, alert: "Author not found."
+      else
+        @post = author.posts.find_by(id: params[:id])
+        redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+      end
+    else
+      @post = Post.find(params[:id])
+    end
+  end 
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
-  end
+    params.require(:post).permit(:title, :description, :author_id)
+  end 
 end
